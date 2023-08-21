937be493818b362d99d8579c216f57c1050ece5f
losiik <49565548+losiik@users.noreply.github.com>
2023-08-21 20:30:28 +0400

    init project

---
+++ backend/handmade_by_semion/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for handmade_by_semion project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'handmade_by_semion.settings')
+
+application = get_asgi_application()
