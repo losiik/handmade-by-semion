50bc1468cbd430c76dc50fb5342fed84e3c4a699
losiik <49565548+losiik@users.noreply.github.com>
2023-09-05 01:32:31 +0400

    start generate api

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path('main_page/', views.PageAboutView.as_view())
+    path('main_page/', views.PageAboutView.as_view()),
+    path('contacts_page/', views.PageContactsView.as_view()),
+    path('send_email_setings/', views.SendEmailSettingsView.as_view()),
+    path('send_email/', views.EmailMessageView.as_view()),
+    path('all_skills/', views.SkillsView.as_view()),
+    path('all_tags/', views.GetAllTags.as_view())
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,15 +1,93 @@
-from django.shortcuts import render
+from django.forms import model_to_dict
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import PageAbout
-from core.serializers import PageAboutSerializer
+from .models import PageAbout, PageContacts, SendEmailSettings, Skills, Tag
+from .send_mail import SendMail
+from .serializers import PageAboutSerializer, PageContactsSerializer, SendEmailSettingsSerializer, \
+    EmailMessageSerializer, SkillsSerializer, TagSerializer
 
 
 class PageAboutView(APIView):
     queryset = PageAbout.objects.all()
     serializer_class = PageAboutSerializer
 
+    @csrf_exempt
     def get(self, request):
         main_page = PageAbout.objects.all()
         serializer = PageAboutSerializer(main_page, many=True)
         return Response({"about": serializer.data})
+
+
+class PageContactsView(APIView):
+    queryset = PageContacts.objects.all()
+    serializer_class = PageContactsSerializer
+
+    @csrf_exempt
+    def get(self, request):
+        contacts_page = PageContacts.objects.all()
+        serializer = PageContactsSerializer(contacts_page, many=True)
+        return Response({"contacts": serializer.data})
+
+
+class SendEmailSettingsView(APIView):
+    queryset = SendEmailSettings.objects.all()
+    serializer_class = SendEmailSettingsSerializer
+
+
+class EmailMessageView(APIView):
+    serializer_class = EmailMessageSerializer
+
+    @csrf_exempt
+    def post(self, request):
+        queryset_email_cred = SendEmailSettings.objects.all()
+        email_creds = model_to_dict(queryset_email_cred[0])
+
+        message = request.data['message']
+        send_mail = SendMail(host=email_creds['host'],
+                             port=email_creds['port'],
+                             email_address_from=email_creds['email_address_from'],
+                             email_password=email_creds['email_password'],
+                             email_address_to=email_creds['email_address_to'],
+                             message=message)
+
+        return Response({"Send email success": send_mail.send_email()})
+
+
+class SkillsView(APIView):
+    queryset = PageContacts.objects.all()
+    serializer_class = PageContactsSerializer
+
+    @csrf_exempt
+    def get(self, request):
+        skills = Skills.objects.all()
+        serializer = SkillsSerializer(skills, many=True)
+        return Response({"skills": serializer.data})
+
+
+class GetAllTags(APIView):
+    @csrf_exempt
+    def get(self, request):
+        response = {}
+
+        skills = Skills.objects.all()
+        skills_serializer = SkillsSerializer(skills, many=True)
+
+        for skill in skills_serializer.data:
+            tags = Tag.objects.filter(skill=skill['id'])
+            tags_serializer = TagSerializer(tags, many=True)
+            response[skill['skill']] = []
+            for tag in tags_serializer.data:
+                response[skill['skill']].append(
+                    {
+                        'id': tag['id'],
+                        'tag': tag['tag']
+                    }
+                )
+
+        return Response({"all_tags": response})
+
+
+class TagView(APIView):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
