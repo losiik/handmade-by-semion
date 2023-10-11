e1dcf60ddf7ddec3d30d38b1ef47da8406c6cd3d
1308267@gmail.com <1308267@gmail.com>
2023-10-11 22:18:49 +0300

    node js server fixed

--- private/apiHandler.js
+++ private/apiHandler.js
@@ -1,33 +1,32 @@
 const axios = require('axios');
 
-async function getDataFromAPI(apiMethod, protocol, host, itemName) {
-    const fullLocPath =  protocol + '//' + host;
+async function getDataFromAPI(apiMethod, itemName) {
   try {
     let apiUrl;
     switch (apiMethod) {
         case 'about':
-            apiUrl = fullLocPath + '/api/main_page/';
+            apiUrl = 'http://localhost:8000/api/main_page/';
             break;
         case 'projects':
-            apiUrl = fullLocPath + '/api/projects/';
+            apiUrl = 'http://localhost:8000/api/projects/';
             break;
         case 'project_name':
-            apiUrl = fullLocPath + '/api/project_full_info/?project=' + itemName;
+            apiUrl = `http://localhost:8000/api/project_full_info/?project=${itemName}`;
             break;
         case 'skills':
-            apiUrl = fullLocPath + '/api/all_skills/';
+            apiUrl = 'http://localhost:8000/api/all_skills/';
             break;
         case 'skill_name':
-            apiUrl = fullLocPath + '/api/skill_full_info/?skil=' + itemName;
+            apiUrl = `http://localhost:8000/api/skill_full_info/?skil=${itemName}`;
             break;
         case 'all_tags':
-            apiUrl = fullLocPath + '/api/all_tags/';
+            apiUrl = 'http://localhost:8000/api/all_tags/';
             break;
         case 'tags':
-            apiUrl = fullLocPath + '/api/projects/?tag=' + itemName;
+            apiUrl = `http://localhost:8000/api/projects/?tag=${itemName}`;
             break;
         case 'contacts':
-            apiUrl = fullLocPath + '/api/contacts_page/';
+            apiUrl = 'http://localhost:8000/api/contacts_page/';
             break;
         default:
             throw new Error('Неизвестный метод API');
--- private/app.js
+++ private/app.js
@@ -3,7 +3,6 @@ const { getDataFromAPI } = require('./apiHandler');
 const ejs = require('ejs');
 const path = require('path');
 
-
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -12,28 +11,30 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', async (req, res) => {
-    const protocol = req.protocol; // 'http' или 'https'
-    const host = req.get('host');  //
+  const protocol = req.protocol;
+  const host = req.get('host'); 
+
   try {
-    const apiData = await getDataFromAPI('about', protocol, host); 
-    const additionalApiData = await getDataFromAPI('all_skills', protocol, host); 
-    const contactsApiData = await getDataFromAPI('contacts', protocol, host); 
+    const apiData = await getDataFromAPI('about'); 
+    const additionalApiData = await getDataFromAPI('all_skills'); 
+    const contactsApiData = await getDataFromAPI('contacts'); 
     res.render('index', { apiData, additionalApiData, contactsApiData, host, protocol });
   } catch (error) {
     console.error('Ошибка при получении данных с API:', error);
-    res.status(500).send('Внутренняя ошибка сервера');
+    res.status(500).render('error',{ errorCode: 500});
   }
+
 });
 
 app.get('/projects', async (req, res) => {
     const protocol = req.protocol; // 'http' или 'https'
     const host = req.get('host');  //
   try {
-    const apiData = await getDataFromAPI('projects', protocol, host); 
-    res.render('project', { apiData });
+    const apiData = await getDataFromAPI('projects'); 
+    res.render('project', { apiData,host, protocol});
   } catch (error) {
     console.error('Ошибка при получении данных с API:', error);
-    res.status(500).send('Внутренняя ошибка сервера');
+    res.status(500).render('error',{ errorCode: 500});
   }
 });
 
@@ -42,12 +43,12 @@ app.get('/projects/?tag=:tag', async (req, res) => {
     const host = req.get('host');  //
     const { tag } = req.params;
   try {
-    const apiData = await getDataFromAPI('tag', protocol, host, tag); 
-    const tagData = await getDataFromAPI('all_tags', protocol, host); 
-    res.render('project', { apiData, tagData });
+    const apiData = await getDataFromAPI('tag', tag); 
+    const tagData = await getDataFromAPI('all_tags'); 
+    res.render('project', { apiData, tagData,host, protocol  });
   } catch (error) {
     console.error('Ошибка при получении данных с API:', error);
-    res.status(500).send('Внутренняя ошибка сервера');
+    res.status(500).render('error',{ errorCode: 500});
   }
 });
 
@@ -56,11 +57,11 @@ app.get('/projects/:project_name', async (req, res) => {
     const host = req.get('host');  //
     const { project_name } = req.params;
   try {
-    const apiData = await getDataFromAPI('project_item', protocol, host, project_name); 
-    res.render(`project_item`, { apiData });
+    const apiData = await getDataFromAPI('project_item', project_name); 
+    res.render(`project_item`, { apiData, host, protocol });
   } catch (error) {
     console.error('Ошибка при получении данных с API:', error);
-    res.status(500).send('Внутренняя ошибка сервера');
+    res.status(500).render('error',{ errorCode: 500});
   }
 });
 
@@ -68,11 +69,11 @@ app.get('/skills', async (req, res) => {
     const protocol = req.protocol; // 'http' или 'https'
     const host = req.get('host');  //
   try {
-    const apiData = await getDataFromAPI('skills', protocol, host); 
-    res.render(`skills`, { apiData });
+    const apiData = await getDataFromAPI('skills'); 
+    res.render(`skills`, { apiData, host, protocol  });
   } catch (error) {
     console.error('Ошибка при получении данных с API:', error);
-    res.status(500).send('Внутренняя ошибка сервера');
+    res.status(500).render('error',{ errorCode: 500});
   }
 });
 
@@ -81,14 +82,18 @@ app.get('/skills/:skills_item', async (req, res) => {
     const host = req.get('host');  //
     const { skills_item }= req.params;
   try {
-    const apiData = await getDataFromAPI('skills_item', protocol, host, skills_item); 
-    res.render(`skill_item`, { apiData });
+    const apiData = await getDataFromAPI('skills_item', skills_item); 
+    res.render(`skill_item`, { apiData, host, protocol });
   } catch (error) {
     console.error('Ошибка при получении данных с API:', error);
-    res.status(500).send('Внутренняя ошибка сервера');
+    res.status(500).render('error',{ errorCode: 500});
   }
 });
 
-app.listen(3000, () => {
-  console.log('Сервер запущен на порту 3000');
+app.use((req, res, next) => {
+  res.status(404).render('error', { errorCode: 404});
+});
+
+app.listen(80, () => {
+  console.log('Сервер запущен на порту 80');
 });
