cc82635c0a5a43d788677323b1ade27b95f9fb39
losiik <49565548+losiik@users.noreply.github.com>
2023-10-19 01:30:45 +0400

    add postgres

--- backend/handmade_by_semion/settings.py
+++ backend/handmade_by_semion/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -56,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'handmade_by_semion.urls'
