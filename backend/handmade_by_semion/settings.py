b142a3efbfdb131d105f2e0d93a8333dd263d191
losiik <49565548+losiik@users.noreply.github.com>
2023-10-27 02:24:19 +0400

    add ssl

--- backend/handmade_by_semion/settings.py
+++ backend/handmade_by_semion/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'rest_framework',
-    #'corsheaders'
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'handmade_by_semion.urls'
